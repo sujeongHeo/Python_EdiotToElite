eb8114ab30ab637378004a029692e7b3948f3773
hsj <41574104+sujeongHeo@users.noreply.github.com>
2020-01-07 04:27:02 +0900

    Update max_date.sql

--- 20-01-07/max_date.sql
+++ 20-01-07/max_date.sql
@@ -1,6 +1,7 @@
-<!-- 문제-->
+<!-- 문제 -->
 <!-- 가장 최근에 들어온 동물은 언제 들어왔는지 조회하는 SQL 문을 작성해주세요. -->
-
+<!-- 문제 출처 -->
+<!-- https://programmers.co.kr/learn/courses/30/lessons/59038 -->
 <!-- 방법1 --> 
 SELECT MAX(DATETIME) FROM ANIMAL_INS
 <!-- 방법2 -->
