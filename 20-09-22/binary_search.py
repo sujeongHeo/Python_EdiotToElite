492a65121fd28d361ad972504fda91d715a022dd
sujeongHeo <gjtnwjd40@gmail.com>
2020-09-26 14:01:13 +0900

    이진 탐색 문풀

--- 20-09-22/binary_search.py
+++ 20-09-22/binary_search.py
@@ -14,13 +14,13 @@ def binary_search(array, target, start, end):
         return binary_search(array, target, mid + 1, end)
 
     # n (원소의 갯수) 와 target ( 찾고자 하는 문자열 )을 입력받기
-    n, target = list(map(int, input().split()))
-    # 전체 원소 입력 받기
-    array = list(map(int, input().split()))
+n, target = list(map(int, input().split()))
+# 전체 원소 입력 받기
+array = list(map(int, input().split()))
 
-    # 이진 탐색 수행 결과 출력 
-    result = binary_search(array, target, 0, n - 1)
-    if result == None:
-        print("원소가 존재하지 않습니다")
-    else:
-        print(result + 1)
+# 이진 탐색 수행 결과 출력 
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("원소가 존재하지 않습니다")
+else:
+    print(result + 1)
